d2e1ff6d39201b604e02e4be315c7f6f00016b57
5j9 <5j9@users.noreply.github.com>
2024-04-05 10:43:19 +0330

    tests: add tests for calendar.ut.ac.ir data

---
+++ tests/test_start_of_years.py
@@ -0,0 +1,35 @@
+from functools import partial
+from pathlib import Path
+from pandas import read_csv, Timestamp, Timedelta
+
+from gshconverter import gregorian_to_solar_hijri, solar_hijri_to_gregorian
+
+
+testdata = Path(__file__).parent / 'testdata'
+
+df = read_csv(
+    testdata / 'kabise.txt',
+    sep=' ',
+    comment='#',
+    names=['shYear', 'gDate'],
+    parse_dates=['gDate'],
+)
+
+df['leapType'] = (
+    df['shYear'].str.contains(r'\d\*$') * 4
+    + df['shYear'].str.contains(r'\d\*\*$') * 5
+)
+
+df['shYear'] = df['shYear'].str.rstrip('*').astype('uint16')
+
+df['shConverted'] = (
+    df['shYear']
+    .map(partial(solar_hijri_to_gregorian, sh_month=1, sh_day=1))
+    .map(lambda x: Timestamp(*x))
+)
+
+assert (df['shConverted'] == df['gDate']).all()
+assert (
+    df['gDate'].map(lambda x: gregorian_to_solar_hijri(x.year, x.month, x.day))
+    == df['shYear'].map(lambda y: (y, 1, 1))
+).all()
